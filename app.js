63d0999088cc786b24c30671eba3c0f6f3b877e6
ro88900721 <alvarez.rocio@yahoo.com.ar>
2021-10-13 18:35:59 -0300

    register

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const puerto=3030
 app.use(express.static(path.resolve(__dirname, './public')));
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, '/views/home.html'))
+    res.sendFile(path.join(__dirname, './views/home.html'))
 })
+
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname, './views/register.html'))
+})
+
 app.listen(puerto, ()=>{
     console.log ("servidor escuchando")
 })
