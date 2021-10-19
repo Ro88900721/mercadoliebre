6ee0affa5bf3c1d15f1c4361528e7db83f250bbb
ro88900721 <alvarez.rocio@yahoo.com.ar>
2021-10-18 22:14:57 -0300

    cambios

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.get('/',(req,res)=>{
 app.get('/register',(req,res)=>{
     res.sendFile(path.join(__dirname, './views/register.html'))
 })
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname, './views/login.html'))
+})
 
 app.listen(puerto, ()=>{
     console.log ("servidor escuchando")
