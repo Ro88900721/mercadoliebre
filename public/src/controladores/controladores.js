30c3667120307344c639e87fa3fa34d2b5c79da1
Ro88900721 <90660522+Ro88900721@users.noreply.github.com>
2022-03-28 20:27:02 -0300

    ordenado

---
+++ public/src/controladores/controladores.js
@@ -0,0 +1,14 @@
+
+const controlador={
+index: (req,res)=>{
+    res.sendFile(__dirname +  '/../src/views/home.html');
+},
+register: (req,res)=>{
+    res.sendFile(__dirname + '/../register.html');
+},
+login: (req,res)=>{
+    res.sendFile(__dirname + '/../login.html');
+},
+}
+
+module.exports = controlador 
